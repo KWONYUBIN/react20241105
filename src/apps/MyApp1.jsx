b331d603e5379b33c75efa46ea9bfeafea30eb00
KWONYUBIN <71390019+KWONYUBIN@users.noreply.github.com>
2024-11-05 16:19:12 +0900

    react 맛보기

---
+++ src/apps/MyApp1.jsx
@@ -0,0 +1,16 @@
+function MyApp1() {
+  return (
+    <div>
+      첫번째 app
+      <p>Lorem ipsum dolor.</p>
+      <ul>
+        <li>Lorem.</li>
+        <li>Ipsam.</li>
+        <li>Est?</li>
+      </ul>
+      <h3>Lorem.</h3>
+    </div>
+  );
+}
+
+export default MyApp1;
