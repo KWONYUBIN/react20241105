1512cb6d03f7c481c9941e7ba6917c5c69ba36b6
KWONYUBIN <71390019+KWONYUBIN@users.noreply.github.com>
2024-11-07 15:30:09 +0900

    상태변경

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
-import App33 from "./practice/App33.jsx";
+import MyApp36 from "./apps/MyApp36.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <App33 />
+      <MyApp36 />
     </Provider>
   </StrictMode>,
 );
