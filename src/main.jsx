99113fa1455a58c4cc0f090279630d299f1433f5
KWONYUBIN <71390019+KWONYUBIN@users.noreply.github.com>
2024-11-07 14:17:34 +0900

    event

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
-import MyApp32 from "./apps/MyApp32.jsx";
+import App33 from "./practice/App33.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <MyApp32 />
+      <App33 />
     </Provider>
   </StrictMode>,
 );
