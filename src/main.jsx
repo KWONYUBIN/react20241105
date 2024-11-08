6cdcdbde93f39960dd7e0100875b30aa6ca24232
KWONYUBIN <71390019+KWONYUBIN@users.noreply.github.com>
2024-11-08 11:27:10 +0900

    state 가 객체(object, list)면 새 객체를 만들어서(깊은 복사) 상태를 변경해야함

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
-import MyApp41 from "./apps/MyApp41.jsx";
+import App43 from "./practice/App43.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <MyApp41 />
+      <App43 />
     </Provider>
   </StrictMode>,
 );
