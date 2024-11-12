6fb306689a32105d20db64e27a0acc09f740a5d9
KWONYUBIN <71390019+KWONYUBIN@users.noreply.github.com>
2024-11-12 17:01:01 +0900

    d

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
-import App49 from "./practice/App49.jsx";
+import App51 from "./practice/App51.jsx";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
   <Provider>
-    <App49 />
+    <App51 />
   </Provider>,
   // </StrictMode>,
 );
