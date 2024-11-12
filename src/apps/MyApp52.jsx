4c49e3d44ead315db172c9700b4aeeb152d55f97
KWONYUBIN <71390019+KWONYUBIN@users.noreply.github.com>
2024-11-12 17:49:04 +0900

    43 분부터 강의 보기

---
+++ src/apps/MyApp52.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { Button } from "../components/ui/button.jsx";
+import { Switch } from "../components/ui/switch.jsx";
+import { Box } from "@chakra-ui/react";
+
+// 컴퍼넌트의 lifecycle
+// initial render (mount)
+// re-render (update)
+// unmount
+
+function MyApp52(props) {
+  const [show, setShow] = useState(true);
+  return (
+    <div>
+      <Switch
+        checked={show}
+        onCheckedChange={(e) => setShow(e.checked)}
+      ></Switch>
+      {show && <MyComp1 />}
+    </div>
+  );
+}
+
+function MyComp1() {
+  const [count, setCount] = useState(0);
+
+  console.log("그림");
+
+  return (
+    <Box>
+      <Button onClick={() => setCount(count + 1)}> count : {count}</Button>
+    </Box>
+  );
+}
+
+export default MyApp52;
