7f38c53d1b3e8a2c13e9deba8fbad530de37fdf5
KWONYUBIN <rnjsdbqls112@naver.com>
2024-11-13 03:16:57 +0900

    s

--- src/apps/MyApp52.jsx
+++ src/apps/MyApp52.jsx
@@ -1,36 +1,41 @@
-import React, { useState } from "react";
-import { Button } from "../components/ui/button.jsx";
-import { Switch } from "../components/ui/switch.jsx";
-import { Box } from "@chakra-ui/react";
+import React, {useState} from "react";
+import {Button} from "../components/ui/button.jsx";
+import {Switch} from "../components/ui/switch.jsx";
+import {Box} from "@chakra-ui/react";
 
 // 컴퍼넌트의 lifecycle
-// initial render (mount)
-// re-render (update)
-// unmount
+// initial render (mount) (떨어졌다가 다시 붙는 것 ex) 스위치를 켬) (새로고침)
+// re-render (update) mount 상태에서 상태가 변하면 re-render 됨
+// unmount : 사라지는 것 (날아가버림)
 
 function MyApp52(props) {
-  const [show, setShow] = useState(true);
-  return (
-    <div>
-      <Switch
-        checked={show}
-        onCheckedChange={(e) => setShow(e.checked)}
-      ></Switch>
-      {show && <MyComp1 />}
-    </div>
-  );
+    const [show, setShow] = useState(true);
+    return (
+        <div>
+            <Switch
+                checked={show}
+                onCheckedChange={(e) => setShow(e.checked)}
+            ></Switch>
+            {show && <MyComp1/>}   {/* unmount 됨 */}
+
+            <hr/>
+            <Box display={show ? "block" : "none"}>
+                <MyComp1/>     {/* unmount 안됨*/}
+            </Box>
+        </div>
+    );
 }
 
 function MyComp1() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  console.log("그림");
+    console.log("그림");
 
-  return (
-    <Box>
-      <Button onClick={() => setCount(count + 1)}> count : {count}</Button>
-    </Box>
-  );
+    return (
+        <Box>
+            <Button onClick={() => setCount(count + 1)}> count : {count}</Button>
+        </Box>
+    );
 }
 
 export default MyApp52;
