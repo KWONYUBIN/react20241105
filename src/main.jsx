fae95b2094c52e378c8ad37804c07894e5f0bb84
KWONYUBIN <71390019+KWONYUBIN@users.noreply.github.com>
2024-11-13 14:14:34 +0900

    link

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
-import App70 from "./practice/App70.jsx";
+import MyApp72 from "./apps/MyApp72.jsx";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
   <Provider>
-    <App70 />
+    <MyApp72 />
   </Provider>,
   // </StrictMode>,
 );
---
+++ src/practice/App72.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App72(props) {
+  return <div></div>;
+}
+
+export default App72;
