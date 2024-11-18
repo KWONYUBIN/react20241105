03438c74bc987068671452a8be8198129dac7403
KWONYUBIN <71390019+KWONYUBIN@users.noreply.github.com>
2024-11-18 17:33:34 +0900

    JWT예제

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
-import App76 from "./practice/App76.jsx";
+import MyApp80 from "./apps/MyApp80.jsx";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
   <Provider>
-    <App76 />
+    <MyApp80 />
   </Provider>,
   // </StrictMode>,
 );
