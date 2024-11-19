f4e7b005fa5cb4149b265237e4ead192f088cd63
KWONYUBIN <71390019+KWONYUBIN@users.noreply.github.com>
2024-11-19 12:07:59 +0900

    화면에서 jwt 디코딩해서 정보얻기

--- src/apps/MyApp80.jsx
+++ src/apps/MyApp80.jsx
@@ -1,16 +1,133 @@
 import React from "react";
 import { Button } from "../components/ui/button.jsx";
 import axios from "axios";
+import { jwtDecode } from "jwt-decode";
 
 function MyApp80(props) {
   return (
     <div>
       <Button
         onClick={() => {
-          axios.get("/api/main9/sub1");
+          const token = localStorage.getItem("token");
+          const decoded = jwtDecode(token);
+          console.log(decoded);
+          console.log("id", decoded.sub);
+          console.log("권한", decoded.scope);
+          console.log("유효기간", new Date(decoded.exp * 1000));
+          console.log("발행시간", new Date(decoded.iat * 1000));
         }}
       >
-        jwt 얻기
+        jwt 디코딩 해서 보기
+      </Button>
+      <hr />
+      <Button
+        onClick={() => {
+          axios.get("/api/main9/sub10", {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          });
+        }}
+      >
+        사용자 정보
+      </Button>
+      <hr />
+      <Button
+        onClick={() => {
+          axios.get("/api/main9/sub4").then((res) => {
+            localStorage.setItem("token", res.data);
+          });
+        }}
+      >
+        어드민
+      </Button>
+      <Button
+        onClick={() => {
+          axios.get("/api/main9/sub5").then((res) => {
+            localStorage.setItem("token", res.data);
+          });
+        }}
+      >
+        매니저
+      </Button>
+      <Button
+        onClick={() => {
+          axios.get("/api/main9/sub6").then((res) => {
+            localStorage.setItem("token", res.data);
+          });
+        }}
+      >
+        어드민 매니저
+      </Button>
+      <Button
+        onClick={() => {
+          axios.get("/api/main9/sub7", {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          });
+        }}
+      >
+        어드민만 가능 요청
+      </Button>
+      <Button
+        onClick={() => {
+          axios.get("/api/main9/sub8", {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          });
+        }}
+      >
+        매니져만 가능 요청
+      </Button>
+      <Button
+        onClick={() => {
+          axios.get("/api/main9/sub9", {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          });
+        }}
+      >
+        어드민/매니져만 가능 요청
+      </Button>
+      <hr />
+      <Button
+        onClick={() => {
+          axios.get("/api/main9/sub1").then((res) => {
+            const token = res.data;
+            // console.log(token);
+            localStorage.setItem("token", token);
+          });
+        }}
+      >
+        jwt 얻기(로그인)
+      </Button>
+      <Button
+        onClick={() => {
+          localStorage.removeItem("token");
+        }}
+      >
+        로그아웃
+      </Button>
+      <Button
+        onClick={() => {
+          axios.get("/api/main9/sub2");
+        }}
+      >
+        누구나 접근
+      </Button>
+      <Button
+        onClick={() => {
+          axios.get("/api/main9/sub3", {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          });
+        }}
+      >
+        로그인 유저만 접근
       </Button>
     </div>
   );
